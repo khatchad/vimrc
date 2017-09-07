818858b84eb1bfab0bd461566a9e6cb56c0e9e57
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2017-09-07 16:47:32 -0400

    Add vim-latex to updates.

--- update_plugins.py
+++ update_plugins.py
@@ -51,6 +51,7 @@ vim-flake8 https://github.com/nvie/vim-flake8
 vim-pug https://github.com/digitaltoad/vim-pug
 vim-yankstack https://github.com/maxbrunsfeld/vim-yankstack
 lightline.vim https://github.com/itchyny/lightline.vim
+vim-latex https://github.com/vim-latex/vim-latex
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
