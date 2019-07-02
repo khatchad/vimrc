06da2cbeb883ec43bb82d7d151c87b777de33dbc
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2019-07-02 10:22:36 -0400

    Add color schemes.

--- update_plugins.py
+++ update_plugins.py
@@ -53,6 +53,7 @@ lightline.vim https://github.com/itchyny/lightline.vim
 vim-latex https://github.com/vim-latex/vim-latex
 vim-tex-syntax https://github.com/gerw/vim-tex-syntax
 LaTeX-Box https://github.com/LaTeX-Box-Team/LaTeX-Box
+awesome-vim-colorschemes https://github.com/rafi/awesome-vim-colorschemes
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
