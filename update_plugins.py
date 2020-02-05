f3da0ab705831ab2be01fc101959e92ae9c0f2ba
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2020-02-05 16:34:23 -0500

    Add folding plugin.

--- update_plugins.py
+++ update_plugins.py
@@ -53,6 +53,7 @@ lightline.vim https://github.com/itchyny/lightline.vim
 vim-tex-syntax https://github.com/gerw/vim-tex-syntax
 awesome-vim-colorschemes https://github.com/rafi/awesome-vim-colorschemes
 ultisnips https://github.com/SirVer/ultisnips
+FastFold https://github.com/Konfekt/FastFold
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
