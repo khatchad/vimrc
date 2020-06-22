ced4aa03ddb778d6275cf4f60327122f80142607
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2020-06-22 15:20:18 -0400

    Revert "Add vim-latex."
    This reverts commit e3d5cc02b9756fa69e5a4b260c6bc61d5b92e87a.

--- update_plugins.py
+++ update_plugins.py
@@ -51,7 +51,6 @@ vim-pug https://github.com/digitaltoad/vim-pug
 vim-yankstack https://github.com/maxbrunsfeld/vim-yankstack
 lightline.vim https://github.com/itchyny/lightline.vim
 vim-tex-syntax https://github.com/gerw/vim-tex-syntax
-vim-latex https://github.com/vim-latex/vim-latex
 awesome-vim-colorschemes https://github.com/rafi/awesome-vim-colorschemes
 ultisnips https://github.com/SirVer/ultisnips
 FastFold https://github.com/Konfekt/FastFold
